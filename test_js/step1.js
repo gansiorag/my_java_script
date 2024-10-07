27c9a7d6a2242c38f69c7f9a174af68ef5c1494b
gansior <breslau1962@yandex.ru>
2024-10-07 07:50:45 +0300

    add ch

--- test_js/step1.js
+++ test_js/step1.js
@@ -23,7 +23,7 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
